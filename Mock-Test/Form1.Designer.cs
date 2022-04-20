0cb644dbc5687e04af58d5ae65a6aec42c49d7a6
Vinay Kumar Prashar <vkumarprashar@users.noreply.github.com>
2022-04-20 20:09:12 +0530

    first commit

---
+++ Mock-Test/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Mock_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.charityComboBox = new System.Windows.Forms.ComboBox();
+            this.runnerEmailTextBox = new System.Windows.Forms.TextBox();
+            this.minDate = new System.Windows.Forms.DateTimePicker();
+            this.maxDate = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.spTargetTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Charity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(256, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Runner Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(499, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Registration Min Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(783, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Registration Max Date";
+            // 
+            // charityComboBox
+            // 
+            this.charityComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charityComboBox.FormattingEnabled = true;
+            this.charityComboBox.Location = new System.Drawing.Point(35, 65);
+            this.charityComboBox.Name = "charityComboBox";
+            this.charityComboBox.Size = new System.Drawing.Size(185, 26);
+            this.charityComboBox.Sorted = true;
+            this.charityComboBox.TabIndex = 4;
+            // 
+            // runnerEmailTextBox
+            // 
+            this.runnerEmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runnerEmailTextBox.Location = new System.Drawing.Point(260, 65);
+            this.runnerEmailTextBox.Name = "runnerEmailTextBox";
+            this.runnerEmailTextBox.Size = new System.Drawing.Size(202, 24);
+            this.runnerEmailTextBox.TabIndex = 5;
+            // 
+            // minDate
+            // 
+            this.minDate.Checked = false;
+            this.minDate.CustomFormat = "";
+            this.minDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minDate.Location = new System.Drawing.Point(503, 65);
+            this.minDate.Name = "minDate";
+            this.minDate.ShowCheckBox = true;
+            this.minDate.Size = new System.Drawing.Size(240, 24);
+            this.minDate.TabIndex = 6;
+            this.minDate.Value = new System.DateTime(2022, 4, 20, 18, 15, 15, 0);
+            // 
+            // maxDate
+            // 
+            this.maxDate.Checked = false;
+            this.maxDate.CustomFormat = "";
+            this.maxDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxDate.Location = new System.Drawing.Point(787, 63);
+            this.maxDate.Name = "maxDate";
+            this.maxDate.ShowCheckBox = true;
+            this.maxDate.Size = new System.Drawing.Size(240, 24);
+            this.maxDate.TabIndex = 7;
+            this.maxDate.Value = new System.DateTime(2022, 4, 20, 18, 15, 15, 0);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 224);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(992, 371);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(434, 117);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(157, 38);
+            this.searchBtn.TabIndex = 9;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(451, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Registration Details";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 627);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(390, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total Sponsership Target of Selected Registrations";
+            // 
+            // spTargetTextBox
+            // 
+            this.spTargetTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spTargetTextBox.Location = new System.Drawing.Point(455, 627);
+            this.spTargetTextBox.Name = "spTargetTextBox";
+            this.spTargetTextBox.ReadOnly = true;
+            this.spTargetTextBox.Size = new System.Drawing.Size(100, 27);
+            this.spTargetTextBox.TabIndex = 12;
+            this.spTargetTextBox.TextChanged += new System.EventHandler(this.spTargetTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 676);
+            this.Controls.Add(this.spTargetTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.maxDate);
+            this.Controls.Add(this.minDate);
+            this.Controls.Add(this.runnerEmailTextBox);
+            this.Controls.Add(this.charityComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox charityComboBox;
+        private System.Windows.Forms.TextBox runnerEmailTextBox;
+        private System.Windows.Forms.DateTimePicker minDate;
+        private System.Windows.Forms.DateTimePicker maxDate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox spTargetTextBox;
+    }
+}
+
---
+++ Mock-Test/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mock_Test
+{
+    public partial class Form1 : Form
+    {
+        //DAO Model Intialization
+        MockTestEntities mockTestEntities;
+        public Form1()
+        {
+            InitializeComponent();
+            mockTestEntities = new MockTestEntities();
+            dataGridView1.DataSource = mockTestEntities.Registrations.Select(reg => new
+            {
+                Runner_Email = reg.Runner.Email,
+                Registration_Date_Time = reg.RegistrationDateTime,
+                Charity_Name = reg.Charity.CharityName,
+                Sponsorship_Target = reg.SponsorshipTarget
+            }).ToList();
+
+            
+            int total = dataGridView1.Rows.Cast<DataGridViewRow>()
+                .Sum(t => Convert.ToInt32(t.Cells[3].Value));
+            spTargetTextBox.Text = total.ToString();
+
+            charityComboBox.DataSource = mockTestEntities.Charities.Select(reg => reg.CharityName).ToList();
+            charityComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private void spTargetTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string _charityName = charityComboBox.SelectedItem.ToString();
+            string _runnerEmail = runnerEmailTextBox.Text.ToLower().Trim().ToString();
+            string _regMinDateString = minDate.Text.Trim().ToString();
+            string _regMaxDateString = maxDate.Text.Trim().ToString();
+
+            mockTestEntities = new MockTestEntities();
+            IEnumerable<Registration> list = mockTestEntities.Registrations;
+            if(!string.IsNullOrWhiteSpace(_charityName))
+            {
+                list = list.Where(reg => reg.Charity.CharityName == _charityName).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(_runnerEmail))
+            {
+                list = list.Where(reg => reg.Runner.Email.ToLower().Contains(_runnerEmail)).ToList();
+            }
+
+            if (minDate.Checked == true && !string.IsNullOrWhiteSpace(_regMinDateString))
+            {
+                DateTime _regMinDate = DateTime.Parse(_regMinDateString);
+                list = list.Where(reg => reg.RegistrationDateTime >= _regMinDate).ToList();
+            }
+
+            if (maxDate.Checked == true && !string.IsNullOrWhiteSpace(_regMaxDateString))
+            {
+                DateTime _regMaxDate = DateTime.Parse(_regMaxDateString);
+                list = list.Where(reg => reg.RegistrationDateTime <= _regMaxDate).ToList();
+            }
+
+            
+            dataGridView1.DataSource = list.Select(reg => new
+            {
+                Runner_Email = reg.Runner.Email,
+                Registration_Date_Time = reg.RegistrationDateTime,
+                Charity_Name = reg.Charity.CharityName,
+                Sponsorship_Target = reg.SponsorshipTarget
+            }).ToList();
+
+            int total = dataGridView1.Rows.Cast<DataGridViewRow>()
+                .Sum(t => Convert.ToInt32(t.Cells[3].Value));
+            spTargetTextBox.Text = total.ToString();
+        }
+
+    }
+}
